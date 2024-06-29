8b2c01d8ccca86acfaef71b68102b9468055c3df
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-06-29 11:05:47 +0100

    Marketing Template

--- public/js/script.js
+++ public/js/script.js
@@ -11,3 +11,40 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
+let tabsContainer = document.querySelector("#tabs");
+
+let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
+
+console.log(tabTogglers);
+
+tabTogglers.forEach(function (toggler) {
+  toggler.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let tabName = this.getAttribute("href");
+
+    let tabContents = document.querySelector("#tab-contents");
+
+    for (let i = 0; i < tabContents.children.length; i++) {
+      tabTogglers[i].classList.remove(
+        "hover:border-[#CDF7DC]",
+        "hover:text-[#CDF7DC]",
+        "border-[#1DB954]",
+        "border-b-2"
+      );
+
+      tabContents.children[i].classList.remove("hidden");
+      if ("#" + tabContents.children[i].id === tabName) {
+        continue;
+      }
+      tabContents.children[i].classList.add("hidden");
+    }
+    e.target.classList.add(
+      "hover:border-[#CDF7DC]",
+      "hover:text-[#CDF7DC]",
+      "border-[#1DB954]",
+      "border-b-2"
+    );
+  });
+});
