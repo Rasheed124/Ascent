b3153a81cc42c91ba0878d8c14859a8679bfece7
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-06-29 18:53:23 +0100

    Marketing Template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['"Montserrat"', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [],
 }
